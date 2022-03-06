5c36298be2452cb798736308e842898f6a5c7cfc
Bel-94 <belinda.ntinyari@student.moringaschool.com>
2022-03-06 22:56:02 +0300

    add LICENSE file

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
+function validate() {
+
 let form  = document.getElementById('akan-form')
-let birthday=document.getElementById('birthday')
-let gender=document.getElementById('gender')
-let submitbtn=document.getElementById('submit')
-let results=document.getElementById("result-name")
+let date=document.getElementById('date')
+let gender=document.akannames.gender;
+// let submitbtn=document.getElementById('submit')
+// let results=document.getElementById("result-name")
 
 // var myName= document.getElementById("Bel")
 // document.getElementById("Bel").innerHTML=Date();
 // document.write(Date());
+
+let date=new Date(document.getElementById("date").value);
+let dateday=date.getDay();
 
     let akan_names = {
       male:{
@@ -30,31 +35,72 @@ let results=document.getElementById("result-name")
     }
 
     let weekdays = ["sunday","monday","tuesday","wednesday","thursday","friday","saturday"]
+    let valid=false
 
-    submitbtn.onclick = (event) => {
-      event.preventDefault();
-      let genderValue = gender.option[gender.selectedIndex].value;
-      let birthdayValue = birthday.value;
-  
-      if (!birthdayValue || !genderValue) {
-          alert("all fields are required!")
+    let dateerror=document.getElementById("dateerror");
+    let gendererror=document.getElementById("gendererror");
+
+    let result=document.getElementById("result");
+
+    if(document.getElementById("date").value==""){
+      dateerror.innerHTML= "Kindly enter a valid date";
+      return false;
+    }else{
+      // validating gender
+      for(let i=0;i<gender.length;i++){
+        if(gender[i].checked){
+          valid=true;
+          break
+        }
       }
-  
-      else {
-          // get day of the week from the birthday
-          let officialBirthDay = new Date(birthdayValue)
-          let day = DAYS[officialBirthDay.getDay()];
-  
-          if (genderValue === "male") {
-              let akanName = NAMES.male[day];
-              // console.log(akanName);
-              results.innerHTML = akanName;
+      if(valid){
+        if(gender[i].value=="male"){
+          result.innerHTML="You were born on a "+day+".<br>"+"Your Akan Name is" + male[dateday];
+        }else{
+          if(gender[i].value=="female"){
+            result.innerHTML="You were born on a "+day+".<br>"+"Your Akan Name is " + female[dateday];
           }
-  
-          else {
-              let akanName = NAMES.female[day];
-              // console.log(akanName);
-              results.innerHTML = akanName;
-          }
+        }
+      }else{
+        gendererror.innerHTML="Kindly select gender";
+        return false;
       }
-  }
+    }
+    function refresh(){
+      document.getElementById("dateerror").innerHTML= "";
+      document.getElementById("gendererror").innerHTML= "";
+      document.getElementById("result").innerHTML= "";
+      date.value= "";
+      let else=document.getElementById("gender");
+
+      for(let j=0; j<gender.length;j++)
+      gender[j].checked=false;
+    }
+
+    // submitbtn.onclick = (event) => {
+    //   event.preventDefault();
+    //   let genderValue = gender.option[gender.selectedIndex].value;
+    //   let birthdayValue = birthday.value;
+  
+  //     if (!birthdayValue || !genderValue) {
+  //         alert("all fields are required!")
+  //     }
+  
+  //     else {
+  //         // get day of the week from the birthday
+  //         let officialBirthDay = new Date(birthdayValue)
+  //         let day = DAYS[officialBirthDay.getDay()];
+  
+  //         if (genderValue === "male") {
+  //             let akanName = NAMES.male[day];
+  //             // console.log(akanName);
+  //             results.innerHTML = akanName;
+  //         }
+  
+  //         else {
+  //             let akanName = NAMES.female[day];
+  //             // console.log(akanName);
+  //             results.innerHTML = akanName;
+  //         }
+  //     }
+  // }
